af79d0fbac10ca4c63672286fc90e59b33cdbb07
Loren Davie <loren@axilent.com>
2013-08-01 17:10:21 -0400

    More profile fix

--- ace/client.py
+++ ace/client.py
@@ -79,7 +79,7 @@ def trigger(args):
     
     p = None
     if not args.profile:
-        p = c.profile()
+        p = c.profile()['profile']
     else:
         p = args.profile
     
@@ -94,7 +94,7 @@ def trigger(args):
         if not args.profile:
             changer = randint(0,num_triggers)
             if changer > threshold:
-                p = c.profile()
+                p = c.profile()['profile']
         
         print 'triggering with profile',p,'category',args.category,'action',args.action,'and variables',var_dict
         c.trigger(profile=p,category=args.category,action=args.action,variables=var_dict)
