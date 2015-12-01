26a26333489068f489b1be187145b83fa10a630a
Loren Davie <code@axilent.com>
2015-12-01 11:36:21 -0500

    Fixing method ref

--- ace/plugins/wrangle/commands.py
+++ ace/plugins/wrangle/commands.py
@@ -2,15 +2,14 @@
 Commands for Wrangle.
 """
 from ace import client as ac
-from ace.config import get_cfg, get_env
+from ace.config import get_cfg, get_env, get_library_key
 
 
 def deploy(args):
     """ 
     Deploys matching content.
     """
-    cfg = get_cfg()
-    client = ac.get_client('axilent.library',cfg.get_library_key(args))
+    client = ac.get_client('axilent.library',get_library_key(args))
     
     client.deployallcontent(deployment_target=args.deployment_target,
                             workflow_step_names=args.workflow_steps,
